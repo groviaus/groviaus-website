4f5d455edf8f9c69febc47ab33a8ddaa30a78e4d
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2025-05-18 13:18:59 +0530

    Refactor App component to use LandingPage for better organization; create new LandingPage component that consolidates multiple sections including Hero, Services, AboutUs, Project, Testimonials, Contact, and Footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import HeroSection from "./pages/landingPage/HeroSection";
-import Services from "./pages/landingPage/servicesSection/Services";
-import AboutUs from "./pages/landingPage/aboutSection/AboutUs";
-import Project from "./pages/landingPage/projectsSection/Project";
-import Testimonials from "./pages/landingPage/testimonialSection/Testimonial";
-import Footer from "./pages/footer/Footer";
-import Contact from "./pages/landingPage/contactSection/Contact";
-function App() {
+import LandingPage from "./pages/landingPage/LandingPage";
+const App = () => {
   return (
-      <>
-      <HeroSection />
-      <Services />
-      <AboutUs />
-      <Project />
-      <Testimonials />
-      <Contact />
-      <Footer />
-    </>
-  );
+    <LandingPage />
+  )
 }
 
 export default App;
---
+++ src/pages/landingPage/LandingPage.jsx
@@ -0,0 +1,22 @@
+import HeroSection from "./HeroSection";
+import Services from "./servicesSection/Services";
+import AboutUs from "./aboutSection/AboutUs";
+import Project from "./projectsSection/Project";
+import Testimonials from "./testimonialSection/Testimonial";
+import Footer from "../footer/Footer";
+import Contact from "./contactSection/Contact";
+function LandingPage() {
+  return (
+      <>
+      <HeroSection />
+      <Services />
+      <AboutUs />
+      <Project />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default LandingPage;
